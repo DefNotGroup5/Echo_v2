cc2ce17c55d1d887ae1c28a73194f7314ff05750
Viktor Ivanov <viktiv02@gmail.com>
2023-11-01 11:32:04 +0100

    Meh nothing really

---
+++ C#/gRPCClients/Account/GrpcClientInterfaces/IGrpcUser.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Account.Models;
+
+namespace gRPCClients.Account.GrpcClientInterfaces;
+
+public interface IGrpcUser
+{
+    public Task AddUser(User user);
+    public Task UpdateUser(User user);
+}
