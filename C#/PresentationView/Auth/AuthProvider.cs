d524318ec6fcb9efb9633f9dc3e9531354d49b81
Viktor Ivanov <viktiv02@gmail.com>
2023-11-29 15:47:44 +0100

    Login/Logout Works, so does registering
    F1, F2, F3 Completed

---
+++ C#/PresentationView/Auth/AuthProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using HTTPClients.ClientInterfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PresentationView.Auth;
+
+public class AuthProvider : AuthenticationStateProvider
+{
+    private readonly IUserService _userService;
+
+    public AuthProvider(IUserService userService)
+    {
+        _userService = userService;
+        _userService.OnAuthStateChanged += AuthStateChanged;
+    }
+    
+    
+    
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        ClaimsPrincipal principal = await _userService.GetAuthAsync();
+        
+        return new AuthenticationState(principal);
+    }
+    
+    private void AuthStateChanged(ClaimsPrincipal principal)
+    {
+        NotifyAuthenticationStateChanged(
+            Task.FromResult(
+                new AuthenticationState(principal)
+            )
+        );
+    }
+}
