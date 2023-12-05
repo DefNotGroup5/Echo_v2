81f41d0a490b1340849e44d88c1a2b004ab941a2
cj1234567891011 <117266318+cj1234567891011@users.noreply.github.com>
2023-12-05 10:37:09 +0100

    grpc client

--- C#/Domain/Shopping/Models/Item.cs
+++ C#/Domain/Shopping/Models/Item.cs
@@ -3,10 +3,12 @@ namespace Domain.Account.Models;
 public class Item
 {
     public int Id { get; set; }
+    
     public int SellerId { get; set;}
     public String Name { get; set; }
     public String ImageUrl { get; set; }
     public String Description { get; set; }
+    public int Quantity { get; set; }
     public int Price { get; set; }
     public int Stock { get; set; }
 
