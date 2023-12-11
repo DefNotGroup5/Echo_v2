6f2ba3515319d40a3f995724a1aa442a0d05f388
Viktor Ivanov <viktiv02@gmail.com>
2023-12-11 20:58:52 +0100

    Shopping Cart Not working

---
+++ C#/HTTPClients/ClientInterfaces/IShoppingCartService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Account.Models;
+using Domain.Shopping.Models;
+
+namespace HTTPClients.ClientInterfaces;
+
+public interface IShoppingCartService
+{
+    public ShoppingCart GetShoppingCart();
+    public void UpdateShoppingCart(ShoppingCart shoppingCart);
+    Task AddItemToShoppingCart(Item item, int quantity);
+    Task RemoveItemFromShoppingCart(int itemId);
+    public event Action<ShoppingCart?>? OnShoppingCartChanged;
+}
--- C#/HTTPClients/ClientInterfaces/IUserService.cs
+++ C#/HTTPClients/ClientInterfaces/IUserService.cs
@@ -13,6 +13,7 @@ public interface IUserService
     public Task LogoutAsync();
     public Task<ClaimsPrincipal> GetAuthAsync();
     public Task AddItemToShoppingCart(Item item, int id);
+    public Task RemoveItemFromShoppingCart(int id);
 
     public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
     public event Action<ShoppingCart?>? OnShoppingCartChanged;
