6a745e3adab9a57dbfa0f9ada212530c907c4386
dodoubleg420 <pipo.svehlak@gmail.com>
2023-12-14 14:58:13 +0100

    Update UserServiceImplTests.java

--- account-data-server/src/test/java/via/sdj3/grpcserverexample/UserServiceImplTests.java
+++ account-data-server/src/test/java/via/sdj3/grpcserverexample/UserServiceImplTests.java
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class GrpcServerExampleApplicationTests {
+class UserServiceImplTests {
 
     @Mock
     private UserRepository userRepository;
